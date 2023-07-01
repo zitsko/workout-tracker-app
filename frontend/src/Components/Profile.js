13810ee56cbf72b7ff6c19242238a435b49c3026
zitsko <kostaskotsis87@gmail.com>
2023-07-01 23:32:14 +0300

    update

--- frontend/src/Components/Profile.js
+++ frontend/src/Components/Profile.js
@@ -6,6 +6,7 @@ function Profile() {
   const navigate = useNavigate();
   const [workout, setWorkout] = useState("");
   const [workouts, setWorkouts] = useState([]);
+  const [updatedWorkouts, setUpdatedWorkouts] = useState([]);
 
   const [user, setUser] = useState({
     _id: "",
@@ -15,6 +16,7 @@ function Profile() {
   function getMyWorkouts(userId) {
     axios.get("http://localhost:3636/todo/" + userId).then(({ data }) => {
       setWorkouts(data);
+      setUpdatedWorkouts(new Array(data.length).fill(""));
     });
   }
 
@@ -37,7 +39,7 @@ function Profile() {
     }
   }, []);
 
-  function createWorkout() {
+  function addWorkout() {
     axios
       .post("http://localhost:3636/todo/", {
         title: workout,
@@ -49,6 +51,29 @@ function Profile() {
       });
   }
 
+  function deleteWorkout(workoutId) {
+    axios
+      .delete(`http://localhost:3636/todo/${workoutId}`)
+      .then(() => {
+        getMyWorkouts(user._id);
+      })
+      .catch((error) => {
+        console.log("Error deleting workout:", error);
+      });
+  }
+
+  function updateWorkout(workoutId, index) {
+    const updatedTitle = updatedWorkouts[index];
+    axios
+      .put(`http://localhost:3636/todo/${workoutId}`, { title: updatedTitle })
+      .then(() => {
+        const updatedArray = [...updatedWorkouts];
+        updatedArray[index] = "";
+        setUpdatedWorkouts(updatedArray);
+        getMyWorkouts(user._id);
+      });
+  }
+
   function disconnect() {
     localStorage.removeItem("token");
     navigate("/");
@@ -56,31 +81,10 @@ function Profile() {
 
   return (
     <div className="profile-container">
-      <h1 className="profile-name">Profile of {user.email}</h1>
-      <p className="description-text">
-        Do you want to set a workout routine to stay fit? Add your favorite
-        exercises or workouts below to create a program that fits your needs!
-      </p>
-      <div className="profile-input-button-container">
-        <input
-          type="text"
-          placeholder="Add workouts"
-          value={workout}
-          className="input-workout"
-          onChange={(e) => {
-            setWorkout(e.target.value);
-          }}
-        />
+      <div className="profile-header">
+        <h1 className="profile-name">Profile of {user.email}</h1>
         <button
-          className="profile-btn create-btn"
-          onClick={() => {
-            createWorkout();
-          }}
-        >
-          Add Workout
-        </button>
-        <button
-          className="profile-btn disconnect-btn"
+          className="disconnect-btn"
           onClick={() => {
             disconnect();
           }}
@@ -89,9 +93,63 @@ function Profile() {
         </button>
       </div>
 
+      <p className="description-text">
+        Do you want to set a workout routine to stay fit? Add your favorite
+        exercises or workouts below to create a program that fits your needs!
+      </p>
+      <div className="profile-input-button-container">
+        <input
+          type="text"
+          placeholder="Create your routine..."
+          value={workout}
+          className="input-workout"
+          onChange={(e) => {
+            setWorkout(e.target.value);
+          }}
+        />
+        <button
+          className="add-btn"
+          onClick={() => {
+            addWorkout();
+          }}
+        >
+          Add Workout
+        </button>
+      </div>
+
       <ul className="workout-list">
-        {workouts.map((e) => (
-          <li className="workout-item">{e.title}</li>
+        {workouts.map((e, index) => (
+          <li className="workout-item" key={e._id}>
+            
+            <span className="workout-title">{e.title}</span>
+
+            <input
+              type="text"
+              placeholder="edit here"
+              value={updatedWorkouts[index]}
+              onChange={(e) => {
+                const updatedArray = [...updatedWorkouts];
+                updatedArray[index] = e.target.value;
+                setUpdatedWorkouts(updatedArray);
+              }}
+              onClick={() => updateWorkout(e._id, index)}
+              className="workout-input"
+            />
+
+            <button
+              className="update-btn"
+              onClick={() => updateWorkout(e._id, index)}
+            >
+              Update
+            </button>
+
+            <button
+              className="delete-workout-btn"
+              onClick={() => deleteWorkout(e._id)}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </div>
