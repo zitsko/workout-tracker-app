9819c97f2e51080ef2ff0d0825c732ccc3269ef7
zitsko <kostaskotsis87@gmail.com>
2023-08-19 13:17:31 +0300

    update window confirms

--- frontend/src/Components/Profile.js
+++ frontend/src/Components/Profile.js
@@ -25,6 +25,7 @@ function Profile() {
   const [workouts, setWorkouts] = useState([]);
 
   const [updatedWorkouts, setUpdatedWorkouts] = useState([]);
+  const [editIndex, setEditIndex] = useState(-1); // -1 indicates no edit mode
 
   const [user, setUser] = useState({
     _id: "",
@@ -129,61 +130,12 @@ function resetWorkouts(userId) {
   }
 
   return (
-    <div className="homepage-container">
-      <div className="flex-row profile-header ">
-           <h1 className="profile-name text-title">
-            Welcome {user.email}
-        </h1>
-        <button
-          className="btn disconnect-btn"
-          onClick={() => {
-            disconnect();
-          }}
-        >
-          <FontAwesomeIcon icon={faPowerOff} size="lg" />
-        </button>
-      </div>
+    <ul className="workout-list">
+      {workouts.map((e, index) => (
+        <li className="flex-row workout-item" key={e._id}>
+          <span className="workout-title">{e.title}</span>
 
-      <p className="description-text">
-        Do you want to set a workout routine to stay fit? Add your favorite
-        exercises or workouts below to create a program that fits your needs!
-        Need some inspiration?Check out our{" "}
-        <a onClick={redirectToExerciseLibrary} className="library-link">
-          Exercise Library!
-        </a>
-      </p>
-      <div className="flex-row profile-input-button-container">
-        <input
-          type="text"
-          placeholder="Create your routine..."
-          value={workout}
-          className="border-bottom-input input-workout"
-          onChange={(e) => {
-            setWorkout(e.target.value);
-          }}
-        />
-
-        <button
-          className="btn primary-btn"
-          onClick={() => {
-            addWorkout();
-          }}
-        >
-          <FontAwesomeIcon icon={faPlus} size="lg" />
-         {" "}
-          <FontAwesomeIcon icon={faDumbbell} size="lg" />
-        </button>
-
-        <button className="btn delete-btn" onClick={resetWorkouts}>
-          <FontAwesomeIcon icon={faTrash} size="lg" />
-        </button>
-      </div>
-
-      <ul className="workout-list">
-        {workouts.map((e, index) => (
-          <li className="flex-row workout-item" key={e._id}>
-            <span className="workout-title">{e.title}</span>
-
+          {editIndex === index ? (
             <input
               type="text"
               placeholder="edit here"
@@ -193,23 +145,31 @@ function resetWorkouts(userId) {
                 updatedArray[index] = e.target.value;
                 setUpdatedWorkouts(updatedArray);
               }}
-              onClick={() => updateWorkout(e._id, index)}
               className="workout-input"
             />
+          ) : (
+            <span className="workout-title">{updatedWorkouts[index]}</span>
+          )}
 
-            <button className="btn primary-btn" onClick={() => updateWorkout(e._id, index)}>
-              
-              <FontAwesomeIcon icon={faPencil} size="lg" />
-            </button>
+          <button
+            className="btn primary-btn"
+            onClick={() => {
+              if (editIndex === index) {
+                // Save updates
+                updateWorkout(e._id, index);
+              }
+              setEditIndex(editIndex === index ? -1 : index);
+            }}
+          >
+            <FontAwesomeIcon icon={faPencil} size="lg" />
+          </button>
 
-            <button className="btn delete-btn" onClick={() => deleteWorkout(e._id)}>
-              
-              <FontAwesomeIcon icon={faDeleteLeft} size="lg" />
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
+          <button className="btn delete-btn" onClick={() => deleteWorkout(e._id)}>
+            <FontAwesomeIcon icon={faDeleteLeft} size="lg" />
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 }
 
