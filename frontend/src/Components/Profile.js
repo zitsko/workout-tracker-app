58fd4cac2d605c47a8c09d5e6fa3df3169b949f7
zitsko <kostaskotsis87@gmail.com>
2023-08-19 13:20:06 +0300

    update window confirms

--- frontend/src/Components/Profile.js
+++ frontend/src/Components/Profile.js
@@ -130,12 +130,61 @@ function resetWorkouts(userId) {
   }
 
   return (
-    <ul className="workout-list">
-      {workouts.map((e, index) => (
-        <li className="flex-row workout-item" key={e._id}>
-          <span className="workout-title">{e.title}</span>
+    <div className="homepage-container">
+      <div className="flex-row profile-header ">
+           <h1 className="profile-name text-title">
+            Welcome {user.email}
+        </h1>
+        <button
+          className="btn disconnect-btn"
+          onClick={() => {
+            disconnect();
+          }}
+        >
+          <FontAwesomeIcon icon={faPowerOff} size="lg" />
+        </button>
+      </div>
 
-          {editIndex === index ? (
+      <p className="description-text">
+        Do you want to set a workout routine to stay fit? Add your favorite
+        exercises or workouts below to create a program that fits your needs!
+        Need some inspiration?Check out our{" "}
+        <a onClick={redirectToExerciseLibrary} className="library-link">
+          Exercise Library!
+        </a>
+      </p>
+      <div className="flex-row profile-input-button-container">
+        <input
+          type="text"
+          placeholder="Create your routine..."
+          value={workout}
+          className="border-bottom-input input-workout"
+          onChange={(e) => {
+            setWorkout(e.target.value);
+          }}
+        />
+
+        <button
+          className="btn primary-btn"
+          onClick={() => {
+            addWorkout();
+          }}
+        >
+          <FontAwesomeIcon icon={faPlus} size="lg" />
+         {" "}
+          <FontAwesomeIcon icon={faDumbbell} size="lg" />
+        </button>
+
+        <button className="btn delete-btn" onClick={resetWorkouts}>
+          <FontAwesomeIcon icon={faTrash} size="lg" />
+        </button>
+      </div>
+
+      <ul className="workout-list">
+        {workouts.map((e, index) => (
+          <li className="flex-row workout-item" key={e._id}>
+            <span className="workout-title">{e.title}</span>
+
             <input
               type="text"
               placeholder="edit here"
@@ -145,31 +194,23 @@ function resetWorkouts(userId) {
                 updatedArray[index] = e.target.value;
                 setUpdatedWorkouts(updatedArray);
               }}
+              onClick={() => updateWorkout(e._id, index)}
               className="workout-input"
             />
-          ) : (
-            <span className="workout-title">{updatedWorkouts[index]}</span>
-          )}
 
-          <button
-            className="btn primary-btn"
-            onClick={() => {
-              if (editIndex === index) {
-                // Save updates
-                updateWorkout(e._id, index);
-              }
-              setEditIndex(editIndex === index ? -1 : index);
-            }}
-          >
-            <FontAwesomeIcon icon={faPencil} size="lg" />
-          </button>
+            <button className="btn primary-btn" onClick={() => updateWorkout(e._id, index)}>
+              
+              <FontAwesomeIcon icon={faPencil} size="lg" />
+            </button>
 
-          <button className="btn delete-btn" onClick={() => deleteWorkout(e._id)}>
-            <FontAwesomeIcon icon={faDeleteLeft} size="lg" />
-          </button>
-        </li>
-      ))}
-    </ul>
+            <button className="btn delete-btn" onClick={() => deleteWorkout(e._id)}>
+              
+              <FontAwesomeIcon icon={faDeleteLeft} size="lg" />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
